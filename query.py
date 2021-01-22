3d04e4cbb72fc912363ecfcac186a9e460460f8e
GP <girishji@gmail.com>
2021-01-22 09:04:41 +0100

    added new route for Bottle app

--- query.py
+++ query.py
@@ -1,20 +1,26 @@
-from bottle import route, run, template, debug
+from bottle import route, run, template, request, debug
 from ethdb import Database
+import sqlite3 as sq3
 
-@route('/query')
+@route('/query', method='GET')
 def query_db():
-    db = Database()
-    r = db.cursor().execute('SELECT * FROM Block')
-    #conn = sq3.connect('./ethblockchain.db')
-    #c = conn.cursor()
-    #c.execute()
-    #c.close()
-    return template('query', data=r.fetchone())
-    #return fetchall() # bottle expects a string or list of strings
+    qstr = request.GET.qstring.strip()
+    try: 
+        db = Database()
+        c = db.cursor()
+        result = c.execute(qstr).fetchall()
+        c.close()
+        return template('query', rows=result)
+    except sq3.OperationalError as er:
+        return template('query', rows=[er])
+
 
 # start web server
 # go to http://localhost:8080/query
-#add this at the very end:
 debug(True) # XXX remove this eventually
 run(reloader=True)
 #run()
+
+# SQL
+# PRAGMA table_info(table_name);
+# SELECT txTo, count(txTo) FROM Tx GROUP BY txTo ORDER BY count(txTo) DESC LIMIT 5
